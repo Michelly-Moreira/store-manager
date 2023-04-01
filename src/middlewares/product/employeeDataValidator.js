4efcae86995ebe0b9edce7f8539094cfc99ffc3c
Michelly Moreira <michelly.daiana@hotmail.com>
2023-04-01 13:22:57 -0300

    task 4

--- src/middlewares/product/employeeDataValidator.js
+++ src/middlewares/product/employeeDataValidator.js
@@ -6,7 +6,14 @@ module.exports = (req, res, next) => {
   const sale = req.body;
   const { error } = addProductSchema.validate(sale);
   if (error) {
-    return res.status(400).json({ message: error.message });
+    if (error.details[0].type === 'any.required') {
+      return next({ status: 400, message: error.message });
+    }
+    if (error.details[0].type === 'string.min') {
+      return next({ status: 422, message: error.message });
+    }
+     // return res.status(400).json(error);
+    // return res.status(400).json({ message: error.message });
   }
   next();
 };
--- src/middlewares/product/productsEmployeesValidation.js
+++ src/middlewares/product/productsEmployeesValidation.js
@@ -3,13 +3,13 @@ const { addProductSchema } = require('../../joi/schemaProducts');
 
 module.exports = (req, res, next) => {
   const products = req.body;
-  const employeeArraySchema = Joi.array().items(addProductSchema);
+  const employeeArraySchema = Joi.object().items(addProductSchema);
   const { error } = employeeArraySchema.validate(products);
   if (error) {
     if (error.details[0].type === 'any.required') {
       return next({ status: 400, message: error.message });
     }
-    if (error.details[0].type === 'name.min') {
+    if (error.details[0].type === 'string.min') {
       return next({ status: 422, message: error.message });
     }
   }
